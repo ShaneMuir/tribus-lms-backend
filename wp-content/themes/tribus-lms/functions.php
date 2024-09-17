a88e2004a93f8f3ca47765e3aab48694872e643a
Shane Muirhead <shane.muirhead@tribusdigital.com>
2024-09-17 16:12:10 +0100

    Added some TODOs

--- wp-content/themes/tribus-lms/functions.php
+++ wp-content/themes/tribus-lms/functions.php
@@ -1,5 +1,13 @@
 <?php
 
+//TODO: Think about how we can make the test cases input / output better. More dynamic.
+//TODO: Add fields for difficulty and score since we want to keep track of users progress and rankings.
+//TODO: Add JWT RESI Auth...
+//TODO: Add custom DB table or CPT for keep track of users progress and rank.
+//TODO: Create REST endpoints for completing challenges, check if challenges are complete, updating user score/rank and so on
+//TODO: Style up the backend
+//TODO: Make WP compliant
+
 $functions_dir = get_template_directory().'/lib/';
 
 // Global functions (Make WP Headless)
